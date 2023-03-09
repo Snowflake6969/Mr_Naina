d2bdf36e5ae018eac48d4b5d2192aaddc0a5eb68
Snowflake6969 <119191912+Snowflake6969@users.noreply.github.com>
2023-03-09 21:04:04 +0300

    Add files via upload

---
+++ FrontServlet.java
@@ -0,0 +1,32 @@
+package etu2076.framework.servlet;
+import etu2076.framework.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FrontServlet extends HttpServlet {
+    protected void processRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        String context = req.getContextPath();
+        out.println(context);
+    }
+    
+    
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       processRequest(req,resp);
+    }
+    
+    HashMap<String,Mapping>MappingUrls;
+}
+
